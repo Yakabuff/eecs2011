16ce1c7c9e025eef620cd0d931887c496ab1589d
Jonathan <yekeb00f@gmail.com>
2019-01-17 13:59:06 -0500

    1st

--- Assignment1/src/FileList.java
+++ Assignment1/src/FileList.java
@@ -2,66 +2,65 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Scanner;
 
-public class FileList<E> extends FileContainer implements List<E>{
+public class FileList<E extends Number> extends FileContainer implements List<E>{
 
-	public E[] elements;
+	public List<E> elements;
 	private int size = 0;
-	BufferedWriter output;
-	BufferedReader input;
+	BufferedWriter bw;
+	BufferedReader br;
 	File file;
-	
-	
-	@SuppressWarnings("unchecked")
+	Scanner reader;
+	PrintWriter pw;
+	//PrintWriter tmp;
+
+	FileOutputStream fs;
+
 	public FileList(String fileName){
-		super.fileName = fileName;
-		//Check if file already exists??
-		File tmpFile = new File(fileName);
-		elements = (E[])new Object [size];
-		
-		 
-		
-		if(tmpFile.exists()) {
-			System.out.println(true);
-			//write contents of file into list??
-			
-		}else {
-			System.out.println(false);
-			file = new File(fileName);
-			try {
-				
-				
-				output = new BufferedWriter(new FileWriter(file));          
 
-				}catch(IOException e ){
-					
-				}
-			elements = (E[])new Object [size];
+		elements = new ArrayList<E>(0);
+		file = new File(fileName);
+		try {
+			fs = new FileOutputStream(file);
+			pw = new PrintWriter(fs,true);
 			
-		
+			
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 
-		
+		super.fileName = fileName;
+
+
 
 	}
-	
-	@SuppressWarnings("unchecked")
+
+
 	public FileList() {
-		elements = (E[])new Object [size];
-		file = new File("");
-		
+
+
 	}
-	
-//////////////////////////////////////////////	
+
+	//////////////////////////////////////////////	
 	@Override
 	public String getFileName() {
 		// TODO Auto-generated method stub
@@ -73,112 +72,155 @@ public class FileList<E> extends FileContainer implements List<E>{
 		// TODO Auto-generated method stub
 		return file.getTotalSpace();
 	}
-///////////////////////////////////////////////
+	///////////////////////////////////////////////
+
 	@Override
 	public boolean add(E e) {
 		// TODO Auto-generated method stub
-		this.size = size+ 1;
-		E[] tempElements = elements;
-		
-		elements = (E[])new Object [this.size];
-		
-		for(int i=0; i<tempElements.length; i++) {
-			elements[i] = tempElements[i];
+		elements.add(e);
+		pw = new PrintWriter(fs,true);
 
-		}
+		pw.println(e.getClass().getSimpleName() +" "+e);
 		
 		
-		elements[this.size-1] = e;
+
 		return true;
 	}
 
+
 	@Override
-	public void add(int index, E element) {
+	public void add(int index, E e) {
 		// TODO Auto-generated method stub
-		this.size = elements.length;
-		
-		grow();
-		
-		System.arraycopy(elements, index, elements, index + 1,size - index);
-		elements[index] = element;
-		
-	}
-	
-	private void grow() {
-		int oldSize = elements.length;
-		
-		int newSize = elements.length+1;
-		
-		elements = Arrays.copyOf(elements, newSize);
+		elements.add(index,e);
+
+		try {
+			fs = new FileOutputStream(file);
+			pw = new PrintWriter(fs,true);
+
+			for(int i = 0; i<elements.size(); i++) {
+				pw.println(elements.get(i).getClass().getSimpleName() + " " + elements.get(i));
+				//ps.println(elements.get(i));
+			}
+
+			fs.close();
+			pw.close();
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 
 	}
-	
-	private void write(E e) {
-		
-	}
-	
-	
-	
+
+
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		elements = (E[])new Object[0];
+		elements.clear();
+
+		try {
+			FileOutputStream fs = new FileOutputStream(file);
+			pw = new PrintWriter(fs,true);
+			fs.close();
+		}catch (IOException e1) {
+
+		}
 	}
 	
+	private void print(File file, String input) {
+		pw.println(input);
+		System.out.println("Printing " + input);
+		
+	}
+
+
 	@Override
 	public boolean remove(Object o) {
 		// TODO Auto-generated method stub
-		boolean removed;
 		
-		if(o == null) {
-			for(int i=0; i<elements.length;i++) {
-				if(elements[i]==null) {
-					System.arraycopy(elements, i+1, elements, i, elements.length-i-1);
-					shrink();
-					size--;
-					return true;
+		//File tempFile = new File("tempfile.txt");
+		FileOutputStream fs;
+		try {
+			fs = new FileOutputStream(file);
+			pw = new PrintWriter(fs,true);
+			for(Object n : elements) {
+				if(!n.equals(o)) {
+					
+					print(file,n.getClass().getSimpleName()+" "+n.toString() );
 				}
 			}
-			return true;
-		}else {
-			for(int i=0; i<elements.length;i++) {
-				if(elements[i].equals(o)) {
-					
-					System.arraycopy(elements, i+1, elements, i, elements.length-i-1);
-					//elements[elements.length-1] = null;
-					shrink();
-					size--;
-					
-					return true;
-				}
-			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 
-		return false;
+	
+
+//		try {
+//
+//			FileOutputStream fileTemp = new FileOutputStream(tempFile);
+//
+//			PrintWriter tmp = new PrintWriter(fileTemp, true);
+//			br = new BufferedReader(new FileReader(file));
+//
+//			String currentLine;
+//			while((currentLine = br.readLine()) != null) {
+//
+//				if(!currentLine.equals(o.getClass().getSimpleName()+" "+o.toString())){
+//					System.out.println(currentLine);
+//					tmp.println(currentLine);
+//				}
+//
+//			}
+//			System.gc();
+//			fileTemp.close();
+//			br.close();
+//			pw.close();
+//			
+//			fs.close();
+//			
+//			file = new File(file, fileName);
+//			
+//			if(file.delete()) {
+//				System.out.println("deleted");
+//			}
+//			
+//			tempFile.renameTo(file);
+
+
+//		} catch (IOException e) {
+
+//		}
+
+
+
+		return elements.remove(o);
+
+
 	}
+
 
 	@Override
 	public E remove(int index) {
 		// TODO Auto-generated method stub
-		return null;
+		E removed = elements.get(index);
+		elements.remove(removed);
+
+		return removed;
 	}
-	
-	private void shrink() {
-		int oldSize = elements.length;
-		
-		int newSize = elements.length-1;
-		elements = Arrays.copyOf(elements, newSize);
-	}
-	
+
+
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return elements.length;
+		return elements.size();
 	}
 
+
+
 	//////////////////////////////////////////////
-	
+
 	@Override
 	public boolean addAll(Collection<? extends E> c) {
 		// TODO Auto-generated method stub
@@ -285,8 +327,10 @@ public class FileList<E> extends FileContainer implements List<E>{
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException();
 	}
-	
 
-	
+
+
+
+
 
 }
