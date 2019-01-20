803b22bc5852854bad9e653298fca96556656941
Jonathan <yekeb00f@gmail.com>
2019-01-20 04:45:01 -0500

    new and improved efficient

---
+++ FileList/src/tester.java
@@ -0,0 +1,32 @@
+
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileList list = new FileList("123.list.txt");
+		list.add(1);
+		Integer two = new Integer(2);
+		list.add(two);
+		
+		
+		
+		Integer three = new Integer(3);
+		
+		list.add(three);
+		
+		
+		FileList list2 = new FileList("456.list.txt");
+		list2.add(4);
+		
+		list2.add(5);
+		
+		
+		
+		
+		
+		list2.add(6);
+		
+		
+	}
+
+}
